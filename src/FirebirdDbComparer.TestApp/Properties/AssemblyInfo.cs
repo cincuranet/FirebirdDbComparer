c01a1f161063e3140a36aa352396351ecfc2903b
Jiri Cincura <jiri@x2develop.com>
2018-07-09 11:39:48 +0200

    NuGet from csproj.

--- src/FirebirdDbComparer.TestApp/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("FirebirdDbComparer.TestApp")]
-[assembly: AssemblyProduct("FirebirdDbComparer")]
-[assembly: ComVisible(false)]
-[assembly: Guid("d2b63647-647e-4de3-b0b2-d645f724d8f3")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
