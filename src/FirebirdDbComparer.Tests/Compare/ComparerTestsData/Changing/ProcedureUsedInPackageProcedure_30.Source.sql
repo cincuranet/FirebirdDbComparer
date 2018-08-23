14546851df9b75846ec838733010ddf3b7783dc0
Jiri Cincura <jiri@x2develop.com>
2018-08-23 11:15:32 +0200

    Tests.

---
+++ src/FirebirdDbComparer.Tests/Compare/ComparerTestsData/Changing/ProcedureUsedInPackageProcedure_30.Source.sql
@@ -0,0 +1,24 @@
+﻿set term ^;
+
+create procedure foo(i bigint)
+as
+begin
+end^
+
+create package some_pkg
+as
+begin
+  procedure test;
+end^
+
+create package body some_pkg
+as
+begin
+  procedure test
+  as
+  begin
+    execute procedure foo(1);
+  end
+end^
+
+set term ;^
