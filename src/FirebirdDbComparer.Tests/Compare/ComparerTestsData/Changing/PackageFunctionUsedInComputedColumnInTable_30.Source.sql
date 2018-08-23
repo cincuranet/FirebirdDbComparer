2f3c3ac28037dcdfd56596bba296caabb591492f
Jiri Cincura <jiri@x2develop.com>
2018-08-23 15:26:11 +0200

    Tests.

---
+++ src/FirebirdDbComparer.Tests/Compare/ComparerTestsData/Changing/PackageFunctionUsedInComputedColumnInTable_30.Source.sql
@@ -0,0 +1,21 @@
+﻿set term ^;
+
+create package some_pkg
+as
+begin
+  function test(i int) returns bigint;
+end^
+
+create package body some_pkg
+as
+begin
+  function test(i int) returns bigint
+  as
+  begin
+    return 1;
+  end
+end^
+
+set term ;^
+
+create table test (i int, c computed by (some_pkg.test(i)));;
