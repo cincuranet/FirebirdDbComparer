f120ed23b422da8e86d916bb13c664fa634bfa67
Jiri Cincura <jiri@x2develop.com>
2018-08-23 10:41:40 +0200

    Extra test.

---
+++ src/FirebirdDbComparer.Tests/Compare/ComparerTestsData/Changing/PackageProcedureUsedInProcedure_30.Source.sql
@@ -0,0 +1,24 @@
+﻿set term ^;
+
+create package some_pkg
+as
+begin
+  procedure test(i bigint);
+end^
+
+create package body some_pkg
+as
+begin
+  procedure test(i bigint)
+  as
+  begin
+  end
+end^
+
+create procedure foo
+as
+begin
+  execute procedure some_pkg.test(1);
+end^
+
+set term ;^
---
+++ src/FirebirdDbComparer.Tests/Compare/ComparerTestsData/Changing/PackageProcedureUsedInProcedure_30.Target.sql
@@ -0,0 +1,24 @@
+﻿set term ^;
+
+create package some_pkg
+as
+begin
+  procedure test(i int);
+end^
+
+create package body some_pkg
+as
+begin
+  procedure test(i int)
+  as
+  begin
+  end
+end^
+
+create procedure foo
+as
+begin
+  execute procedure some_pkg.test(1);
+end^
+
+set term ;^
