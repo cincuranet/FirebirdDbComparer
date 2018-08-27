bb143d9f93c0903a2f93e2ba41a1a382d345c5ca
Jiri Cincura <jiri@x2develop.com>
2018-08-27 14:30:00 +0200

    More tests.

---
+++ src/FirebirdDbComparer.Tests/Compare/ComparerTestsData/Creating/PackageFunctionUsingFunction_30.Source.sql
@@ -0,0 +1,25 @@
+﻿set term ^;
+
+create function foo(i int) returns int
+as
+begin
+  return 1;
+end^
+
+create package some_pkg
+as
+begin
+  function test(i int) returns int;
+end^
+
+create package body some_pkg
+as
+begin
+  function test(i int) returns int
+  as
+  begin
+    return foo(i);
+  end
+end^
+
+set term ;^
