6c5bef8fe8206fe87eb511c836c9a83e3fbfcbe1
Jiri Cincura <jiri@x2develop.com>
2018-10-08 14:55:46 +0200

    More tests.

---
+++ src/FirebirdDbComparer.Tests/Compare/ComparerTestsData/Changing/PackageProcedureUsingNewTableNewColumnViaTypeOfColumn_30.Target.sql
@@ -0,0 +1,9 @@
+﻿set term ^;
+
+create package some_pkg
+as
+begin
+  procedure test(i int);
+end^
+
+set term ;^
---
+++ src/FirebirdDbComparer.Tests/Compare/ComparerTestsData/Creating/PackageProcedureUsingNewTableNewColumnViaTypeOfColumn_30.Source.sql
@@ -0,0 +1,11 @@
+﻿create table test (c bigint);
+
+set term ^;
+
+create package some_pkg
+as
+begin
+  procedure test(i type of column test.c);
+end^
+
+set term ;^
