4fe2620133c07dd26e34b07ebd43df2c468cb2ba
Jiri Cincura <jiri@x2develop.com>
2018-11-16 10:27:39 +0100

    New test.

---
+++ src/FirebirdDbComparer.Tests/Compare/ComparerTestsData/Creating/PackageFunctionUsingNewColumnViaTypeOfColumnUsedInNewComputedColumn_30.Source.sql
@@ -0,0 +1,23 @@
+﻿create table test (x bigint, t smallint);
+
+set term ^;
+
+create package some_pkg
+as
+begin
+  function test(x type of column test.t) returns type of column test.t;
+end^
+
+create package body some_pkg
+as
+begin
+  function test(x type of column test.t) returns type of column test.t
+  as
+  begin
+    return x;
+  end
+end^
+
+set term ;^
+
+alter table test add c computed by (some_pkg.test(t));
---
+++ src/FirebirdDbComparer.Tests/Compare/ComparerTestsData/Creating/PackageFunctionUsingNewColumnViaTypeOfColumnUsedInNewComputedColumn_30.Target.sql
@@ -0,0 +1,1 @@
+﻿create table test (x bigint);
