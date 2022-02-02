7efa26165c2bcf670f04ea43d87cdc14e6fcbf79
Jiri Cincura <jiri@x2develop.com>
2022-02-02 13:49:18 +0100

    Support for FB4 for IDENTITY columns.

---
+++ src/FirebirdDbComparer.Tests/Compare/ComparerTestsData/Creating/TableWithIdentityAlwaysWithIncrement.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq;
+using FirebirdDbComparer.Compare;
+using NUnit.Framework;
+
+namespace FirebirdDbComparer.Tests.Compare.ComparerTestsData.Creating;
+
+public class TableWithIdentityAlwaysWithIncrement : ComparerTests.TestCaseStructure
+{
+    public override bool IsCompatibleWithVersion(TargetVersion targetVersion)
+    {
+        return targetVersion.AtLeast(TargetVersion.Version40);
+    }
+
+    public override string Source => @"
+create table t (i int generated always as identity (increment by 16));				
+";
+
+    public override string Target => @"
+
+";
+}
