b2c4c96bb198bb49150a83331b51c99973d3ab28
Jiri Cincura <jiri@x2develop.com>
2022-02-02 09:54:49 +0100

    INT128 test.

---
+++ src/FirebirdDbComparer.Tests/Compare/ComparerTestsData/Creating/TableWithInt128.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq;
+using FirebirdDbComparer.Compare;
+using NUnit.Framework;
+
+namespace FirebirdDbComparer.Tests.Compare.ComparerTestsData.Creating;
+
+public class TableWithInt128 : ComparerTests.TestCaseStructure
+{
+    public override bool IsCompatibleWithVersion(TargetVersion targetVersion)
+    {
+        return targetVersion.AtLeast(TargetVersion.Version40);
+    }
+
+    public override string Source => @"
+create table t (i int128);
+";
+
+    public override string Target => @"
+
+";
+}
