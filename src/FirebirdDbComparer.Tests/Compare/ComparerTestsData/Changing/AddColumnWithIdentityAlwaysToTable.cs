9e51ca9ca60c0dd6ec9e8e35cfc42773dd81737d
Jiri Cincura <jiri@x2develop.com>
2022-02-03 10:01:30 +0100

    Tests.

---
+++ src/FirebirdDbComparer.Tests/Compare/ComparerTestsData/Changing/AddColumnWithIdentityAlwaysToTable.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq;
+using FirebirdDbComparer.Compare;
+using NUnit.Framework;
+
+namespace FirebirdDbComparer.Tests.Compare.ComparerTestsData.Changing;
+
+public class AddColumnWithIdentityAlwaysToTable : ComparerTests.TestCaseStructure
+{
+    public override bool IsCompatibleWithVersion(TargetVersion targetVersion)
+    {
+        return targetVersion.AtLeast(TargetVersion.Version40);
+    }
+
+    public override string Source => @"
+create table t (a int, b bigint generated always as identity);				
+";
+
+    public override string Target => @"
+create table t (a int);
+";
+}
