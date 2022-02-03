df76e37a16d52d345ea22f7edf59e3f57bc15ebf
Jiri Cincura <jiri@x2develop.com>
2022-02-03 12:29:32 +0100

    Drop Castle Windsor.

--- src/FirebirdDbComparer/Interfaces/IComparerFactory.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace FirebirdDbComparer.Interfaces;
-
-public interface IComparerFactory : IDisposable
-{
-    IComparer Create(IComparerSettings comparerSettings, IMetadata sourceMetadata, IMetadata targetMetadata);
-}
