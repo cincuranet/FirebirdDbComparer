7b1940f572d8e688810b805467f013526e925a11
Jiri Cincura <jiri@x2develop.com>
2022-02-08 11:31:25 +0100

    Tests for "grant role to role".

---
+++ src/FirebirdDbComparer.Tests/Compare/ComparerTestsData/Creating/GrantRoleToRole.cs
@@ -0,0 +1,15 @@
+namespace FirebirdDbComparer.Tests.Compare.ComparerTestsData.Creating;
+
+public class GrantRoleToRole : ComparerTests.TestCaseStructure
+{
+    public override string Source => @"
+create role role1;
+create role role2;
+grant role1 to role2;				
+";
+
+    public override string Target => @"
+create role role1;
+create role role2;
+";
+}
