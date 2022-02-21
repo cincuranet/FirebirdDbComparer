682365e2ac73897aaf96c67a49c38901d66a6717
Jiri Cincura <jiri@x2develop.com>
2022-02-21 10:59:13 +0100

    Simpler code

--- src/FirebirdDbComparer/Compare/Comparer.Helpers.cs
+++ src/FirebirdDbComparer/Compare/Comparer.Helpers.cs
@@ -36,76 +36,63 @@ namespace FirebirdDbComparer.Compare
 
         private static Func<Metadata, IReadOnlyCollection<IDatabaseObject>> GetDatabaseObjectFactory(TargetVersion targetVersion, ISqlHelper sqlHelper) => targetVersion switch
         {
-            TargetVersion.Version25 => m => DatabaseObjectFactory25(m, sqlHelper),
-            TargetVersion.Version30 => m => DatabaseObjectFactory30(m, sqlHelper),
-            TargetVersion.Version40 => m => DatabaseObjectFactory40(m, sqlHelper),
+            TargetVersion.Version25 => m => new IDatabaseObject[]
+                {
+                    new MetadataCollations25(m, sqlHelper),
+                    new MetadataConstraints25(m, sqlHelper),
+                    new MetadataDatabase25(m, sqlHelper),
+                    new MetadataDependencies25(m, sqlHelper),
+                    new MetadataExceptions25(m, sqlHelper),
+                    new MetadataFields25(m, sqlHelper),
+                    new MetadataFunctions25(m, sqlHelper),
+                    new MetadataGenerators25(m, sqlHelper),
+                    new MetadataCharacterSets25(m, sqlHelper),
+                    new MetadataIndices25(m, sqlHelper),
+                    new MetadataProcedures25(m, sqlHelper),
+                    new MetadataRelations25(m, sqlHelper),
+                    new MetadataRoles25(m, sqlHelper),
+                    new MetadataTriggers25(m, sqlHelper),
+                    new MetadataUserPrivileges25(m, sqlHelper),
+                },
+            TargetVersion.Version30 => m => new IDatabaseObject[]
+                {
+                    new MetadataCollations30(m, sqlHelper),
+                    new MetadataConstraints30(m, sqlHelper),
+                    new MetadataDatabase30(m, sqlHelper),
+                    new MetadataDependencies30(m, sqlHelper),
+                    new MetadataExceptions30(m, sqlHelper),
+                    new MetadataFields30(m, sqlHelper),
+                    new MetadataFunctions30(m, sqlHelper),
+                    new MetadataGenerators30(m, sqlHelper),
+                    new MetadataCharacterSets30(m, sqlHelper),
+                    new MetadataIndices30(m, sqlHelper),
+                    new MetadataPackages30(m, sqlHelper),
+                    new MetadataProcedures30(m, sqlHelper),
+                    new MetadataRelations30(m, sqlHelper),
+                    new MetadataRoles30(m, sqlHelper),
+                    new MetadataTriggers30(m, sqlHelper),
+                    new MetadataUserPrivileges30(m, sqlHelper),
+                },
+            TargetVersion.Version40 => m => new IDatabaseObject[]
+                {
+                    new MetadataCollations40(m, sqlHelper),
+                    new MetadataConstraints40(m, sqlHelper),
+                    new MetadataDatabase40(m, sqlHelper),
+                    new MetadataDependencies40(m, sqlHelper),
+                    new MetadataExceptions40(m, sqlHelper),
+                    new MetadataFields40(m, sqlHelper),
+                    new MetadataFunctions40(m, sqlHelper),
+                    new MetadataGenerators40(m, sqlHelper),
+                    new MetadataCharacterSets40(m, sqlHelper),
+                    new MetadataIndices40(m, sqlHelper),
+                    new MetadataPackages40(m, sqlHelper),
+                    new MetadataProcedures40(m, sqlHelper),
+                    new MetadataRelations40(m, sqlHelper),
+                    new MetadataRoles40(m, sqlHelper),
+                    new MetadataTriggers40(m, sqlHelper),
+                    new MetadataUserPrivileges40(m, sqlHelper),
+                },
             _ => throw new ArgumentOutOfRangeException(nameof(targetVersion)),
         };
-
-        private static IReadOnlyCollection<IDatabaseObject> DatabaseObjectFactory40(Metadata m, ISqlHelper sqlHelper)
-        {
-            return new IDatabaseObject[]
-            {
-                new MetadataCollations40(m, sqlHelper),
-                new MetadataConstraints40(m, sqlHelper),
-                new MetadataDatabase40(m, sqlHelper),
-                new MetadataDependencies40(m, sqlHelper),
-                new MetadataExceptions40(m, sqlHelper),
-                new MetadataFields40(m, sqlHelper),
-                new MetadataFunctions40(m, sqlHelper),
-                new MetadataGenerators40(m, sqlHelper),
-                new MetadataCharacterSets40(m, sqlHelper),
-                new MetadataIndices40(m, sqlHelper),
-                new MetadataPackages40(m, sqlHelper),
-                new MetadataProcedures40(m, sqlHelper),
-                new MetadataRelations40(m, sqlHelper),
-                new MetadataRoles40(m, sqlHelper),
-                new MetadataTriggers40(m, sqlHelper),
-                new MetadataUserPrivileges40(m, sqlHelper),
-            };
-        }
-        private static IReadOnlyCollection<IDatabaseObject> DatabaseObjectFactory30(Metadata m, ISqlHelper sqlHelper)
-        {
-            return new IDatabaseObject[]
-            {
-                new MetadataCollations30(m, sqlHelper),
-                new MetadataConstraints30(m, sqlHelper),
-                new MetadataDatabase30(m, sqlHelper),
-                new MetadataDependencies30(m, sqlHelper),
-                new MetadataExceptions30(m, sqlHelper),
-                new MetadataFields30(m, sqlHelper),
-                new MetadataFunctions30(m, sqlHelper),
-                new MetadataGenerators30(m, sqlHelper),
-                new MetadataCharacterSets30(m, sqlHelper),
-                new MetadataIndices30(m, sqlHelper),
-                new MetadataPackages30(m, sqlHelper),
-                new MetadataProcedures30(m, sqlHelper),
-                new MetadataRelations30(m, sqlHelper),
-                new MetadataRoles30(m, sqlHelper),
-                new MetadataTriggers30(m, sqlHelper),
-                new MetadataUserPrivileges30(m, sqlHelper),
-            };
-        }
-        private static IReadOnlyCollection<IDatabaseObject> DatabaseObjectFactory25(Metadata m, ISqlHelper sqlHelper)
-        {
-            return new IDatabaseObject[]
-            {
-                new MetadataCollations25(m, sqlHelper),
-                new MetadataConstraints25(m, sqlHelper),
-                new MetadataDatabase25(m, sqlHelper),
-                new MetadataDependencies25(m, sqlHelper),
-                new MetadataExceptions25(m, sqlHelper),
-                new MetadataFields25(m, sqlHelper),
-                new MetadataFunctions25(m, sqlHelper),
-                new MetadataGenerators25(m, sqlHelper),
-                new MetadataCharacterSets25(m, sqlHelper),
-                new MetadataIndices25(m, sqlHelper),
-                new MetadataProcedures25(m, sqlHelper),
-                new MetadataRelations25(m, sqlHelper),
-                new MetadataRoles25(m, sqlHelper),
-                new MetadataTriggers25(m, sqlHelper),
-                new MetadataUserPrivileges25(m, sqlHelper),
-            };
-        }
     }
 }
